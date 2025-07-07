b5ce4bacb4a12d1c5c3a98f40c4eb0276c92bbca
rododecba <rododecba@gmail.com>
2025-07-07 23:11:40 +0200

    Update firebase-config.js

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -1,12 +1,15 @@
-
-// Configuración de Firebase (reemplaza con tus datos reales)
+// === 🔥 CONFIGURACIÓN FIREBASE LIBRE ===
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyC7MKy2T8CFvpay4FBp8FTrVp8tpU0Niwc",
+  authDomain: "libre-c5bf7.firebaseapp.com",
+  projectId: "libre-c5bf7",
+  storageBucket: "libre-c5bf7.appspot.com",
+  messagingSenderId: "339942652190",
+  appId: "1:339942652190:web:595ce692456b9df806f10f"
 };
+
+// Inicializa Firebase
 firebase.initializeApp(firebaseConfig);
+
+// Referencia a Firestore
 const db = firebase.firestore();
